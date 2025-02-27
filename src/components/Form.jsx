401d137d3b60255031f1d338d42a6281906626ba
shaileshdhandhukiya <shaileshdhandhukiya012@gmail.com>
2025-02-27 13:14:10 +0530

    Handle Input Element in React

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,15 +5,16 @@ function Form() {
 
     const [input, setInput] = useState('');
 
-    const handleChanges = (e) => {
-        setInput(e.target.value);
-        console.log(e.target.value);
-    }
+    // const handleChanges = (e) => {
+    //     setInput(e.target.value);
+    //     console.log(e.target.value);
+    // }
 
     return ( 
         <div>
             <form>
-                <input onChange={handleChanges} type="text" />
+                <input onChange={(e)=>setInput(e.target.value)} type="text" value={input}/>
+                <h1>{input}</h1>
             </form>
         </div>
 
